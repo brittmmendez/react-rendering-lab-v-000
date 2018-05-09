33f2cb68ca7c875fe868e352ecd58f3997832e64
brittmmendez <brittmmendez@gmail.com>
2018-05-08 23:31:32 -0400

    Done.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -25,6 +25,9 @@ class Animation extends React.Component {
       );
   };
 
+  componentWillUpdate() {
+    this.showLoadingBar();  
+  }
   showLoadingBar = () => {
     const progressBar = document.getElementById("progress-bar");
     progressBar.className = "off on";
